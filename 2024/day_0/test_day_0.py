7aaee477e894370c73db36043b97d9027eb50e8f
aurechabnv <git@keltpoint.com>
2024-12-01 22:55:24 +0100

    Use pytest

--- 2024/day_0/test_day_0.py
+++ 2024/day_0/test_day_0.py
@@ -1,26 +1,24 @@
-import unittest
+import pytest
 
 from utils import utils
 from day_0 import part1, part2, get_data
 
 
-class MyTestCase(unittest.TestCase):
-    def test_example_part1(self):
-        data = get_data(utils.SRC_EXAMPLE)
-        self.assertEqual(part1(data), True)
+def test_example_part1():
+    data = get_data(utils.SRC_EXAMPLE)
+    assert part1(data) == True
 
-    def test_example_part2(self):
-        data = get_data(utils.SRC_EXAMPLE)
-        self.assertEqual(part2(data), True)
+def test_example_part2():
+    data = get_data(utils.SRC_EXAMPLE)
+    assert part2(data) == True
 
-    def test_input_part1(self):
-        data = get_data(utils.SRC_INPUT)
-        self.assertEqual(part1(data), True)
+def test_input_part1():
+    data = get_data(utils.SRC_INPUT)
+    assert part1(data) == True
 
-    def test_input_part2(self):
-        data = get_data(utils.SRC_INPUT)
-        self.assertEqual(part2(data), True)
-
+def test_input_part2():
+    data = get_data(utils.SRC_INPUT)
+    assert part2(data) == True
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
--- 2024/day_1/test_day_1.py
+++ 2024/day_1/test_day_1.py
@@ -1,26 +1,24 @@
-import unittest
+import pytest
 
 from utils import utils
 from day_1 import part1, part2, get_data
 
 
-class MyTestCase(unittest.TestCase):
-    def test_example_part1(self):
-        list1, list2 = get_data(utils.SRC_EXAMPLE)
-        self.assertEqual(part1(list1, list2), 11)
+def test_example_part1():
+    list1, list2 = get_data(utils.SRC_EXAMPLE)
+    assert part1(list1, list2) == 11
 
-    def test_example_part2(self):
-        list1, list2 = get_data(utils.SRC_EXAMPLE)
-        self.assertEqual(part2(list1, list2), 31)
+def test_example_part2():
+    list1, list2 = get_data(utils.SRC_EXAMPLE)
+    assert part2(list1, list2) == 31
 
-    def test_input_part1(self):
-        list1, list2 = get_data(utils.SRC_INPUT)
-        self.assertEqual(part1(list1, list2), 1834060)
+def test_input_part1():
+    list1, list2 = get_data(utils.SRC_INPUT)
+    assert part1(list1, list2) == 1834060
 
-    def test_input_part2(self):
-        list1, list2 = get_data(utils.SRC_INPUT)
-        self.assertEqual(part2(list1, list2), 21607792)
-
+def test_input_part2():
+    list1, list2 = get_data(utils.SRC_INPUT)
+    assert part2(list1, list2) == 21607792
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
