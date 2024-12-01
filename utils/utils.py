8cad4a60a340b7bf1b23e86ee257d96a7dda210f
aurechabnv <git@keltpoint.com>
2024-12-01 23:26:59 +0100

    Fix doc

--- utils/utils.py
+++ utils/utils.py
@@ -13,20 +13,20 @@ SRC_INPUT = 'input'
 CUR_YEAR = datetime.now().year
 
 
-def get_input(day, year=CUR_YEAR):
+def get_input(day: int, year=CUR_YEAR) -> str:
     response = requests.get(f"https://adventofcode.com/{year}/day/{day}/input",
                             headers={"cookie": "session=" + AOC_COOKIE})
     return response.text
 
 
-def get_example(day, year=CUR_YEAR, offset=0):
+def get_example(day: int, year=CUR_YEAR, offset=0) -> str:
     response = requests.get(f"https://adventofcode.com/{year}/day/{day}",
                             headers={"cookie": "session=" + AOC_COOKIE})
     blocks = response.text.split("<pre><code>")
     return blocks[offset + 1].split("</code></pre>")[0]
 
 
-def get_data(src, day, offset=0) -> (list, list):
+def get_data(src: str, day: int, offset=0) -> str:
     data = ""
     if src == SRC_EXAMPLE:
         data = get_example(day, offset=offset)
