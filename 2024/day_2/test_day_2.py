80f821a3aea676d3ed8279adb5c7a886aebe66d9
aurechabnv <git@keltpoint.com>
2024-12-03 11:17:54 +0100

    Update tests template

--- 2024/day_2/test_day_2.py
+++ 2024/day_2/test_day_2.py
@@ -1,20 +1,20 @@
 import pytest
 
 import aoc
-from day_2 import part1, part2, get_data
+from day_2 import get_data, part1, part2
 
 
 def test_example_part1():
     data = get_data(aoc.SRC_EXAMPLE)
     assert part1(data) == 2
 
-def test_example_part2():
-    data = get_data(aoc.SRC_EXAMPLE)
-    assert part2(data) == 4
-
 def test_input_part1():
     data = get_data(aoc.SRC_INPUT)
     assert part1(data) == 407
+
+def test_example_part2():
+    data = get_data(aoc.SRC_EXAMPLE)
+    assert part2(data) == 4
 
 def test_input_part2():
     data = get_data(aoc.SRC_INPUT)
--- template/day_0/test_day_0.py
+++ template/day_0/test_day_0.py
@@ -1,20 +1,20 @@
 import pytest
 
 from aoc import utils
-from day_0 import part1, part2, get_data
+from day_0 import get_data, part1, part2
 
 
 def test_example_part1():
     data = get_data(utils.SRC_EXAMPLE)
     assert part1(data) == True
 
-def test_example_part2():
-    data = get_data(utils.SRC_EXAMPLE)
-    assert part2(data) == True
-
 def test_input_part1():
     data = get_data(utils.SRC_INPUT)
     assert part1(data) == True
+
+def test_example_part2():
+    data = get_data(utils.SRC_EXAMPLE)
+    assert part2(data) == True
 
 def test_input_part2():
     data = get_data(utils.SRC_INPUT)
