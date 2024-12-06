b7340541dbdfe1e1f87a88534e3d618c677cb3a5
aurechabnv <git@keltpoint.com>
2024-12-06 01:12:35 +0100

    Update aoc import in test files

--- 2024/day_3/test_day_3.py
+++ 2024/day_3/test_day_3.py
@@ -1,32 +1,31 @@
 import pytest
 
 import aoc
-from aoc import utils
 from day_3 import get_data, part1, part2, part2_alt
 
 
 def test_example_part1():
-    data = get_data(utils.SOURCE.EXAMPLE)
+    data = get_data(aoc.SOURCE.EXAMPLE)
     assert part1(data) == 161
 
 def test_input_part1():
-    data = get_data(utils.SOURCE.INPUT)
+    data = get_data(aoc.SOURCE.INPUT)
     assert part1(data) == 188116424
 
 def test_example_part2():
-    data = get_data(utils.SOURCE.EXAMPLE, offset=1)
+    data = get_data(aoc.SOURCE.EXAMPLE, offset=1)
     assert part2(data) == 48
 
 def test_input_part2():
-    data = get_data(utils.SOURCE.INPUT)
+    data = get_data(aoc.SOURCE.INPUT)
     assert part2(data) == 104245808
 
 def test_example_part2_alt():
-    data = get_data(utils.SOURCE.EXAMPLE, offset=1)
+    data = get_data(aoc.SOURCE.EXAMPLE, offset=1)
     assert part2_alt(data) == 48
 
 def test_input_part2_alt():
-    data = get_data(utils.SOURCE.INPUT)
+    data = get_data(aoc.SOURCE.INPUT)
     assert part2_alt(data) == 104245808
 
 if __name__ == '__main__':
