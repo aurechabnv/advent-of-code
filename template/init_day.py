10ccbba70dec4f6ab2aca199fd474eff48312809
aurechabnv <git@keltpoint.com>
2024-12-06 08:01:32 +0100

    Update table in README.md

--- template/init_day.py
+++ template/init_day.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime
 from pathlib import Path
 import shutil
@@ -8,6 +9,9 @@ cur_day = datetime.today().day
 # user inputs
 year = input(f"Year (default {cur_year}) : ") or str(cur_year)
 day = input(f"Day (default {cur_day}) : ") or str(cur_day)
+day_title = ''
+while not day_title:
+    day_title = input(f"Day Title (required) : ").title()
 
 base_path = Path(__file__).resolve().parent.parent
 template_folder = base_path / 'template' / 'day_0'
@@ -26,8 +30,18 @@ for child in template_folder.iterdir():
             content = f.read()
 
         content = content.replace(f"day_0", f"day_{day}") # update import
-        content = content.replace(f"# Day", f"# Day {day}") # update comment
+        content = content.replace(f"# Day", f"# Day {day}: {day_title}") # update comment
         content = content.replace(f"day=0", f"day={day}") # update request
 
         with open(target_folder / file_name, "w") as f:
             f.write(content)
+
+# update table in README.md
+with open(base_path / 'README.md', 'r') as f:
+    content = f.read()
+matches = re.compile(fr'(\| \[Day \d{int(day)-1}: (.*)].*\|)').search(content)
+last_table_line = matches.group(1)
+new_table_line = last_table_line.replace(matches.group(2), day_title).replace(str(int(day)-1), day).replace('⭐','')
+content = content.replace(last_table_line, last_table_line + '\n' + new_table_line)
+with open(base_path / 'README.md', 'w') as f:
+    f.write(content)
