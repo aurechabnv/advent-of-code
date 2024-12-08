0b872e0e54fade6c922cca5bee5954a8bfc4e8b9
aurechabnv <git@keltpoint.com>
2024-12-08 16:32:11 +0100

    Day 8 - Part 2

--- 2024/day_8/day_8.py
+++ 2024/day_8/day_8.py
@@ -1,5 +1,6 @@
 # Day 8: Resonant Collinearity
 
+from collections import defaultdict
 from functools import partial
 
 import aoc
@@ -10,56 +11,70 @@ def get_data(source):
     return [list(item) for item in data.splitlines()]
 
 
-def show_map(data):
-    print('\n'.join([''.join(row) for row in data]))
-
-
 def get_antennas(data):
-    antennas = {}
+    antennas = defaultdict(list)
     for row in data:
         for cell in row:
             if cell != '.':
-                if cell not in antennas:
-                    antennas[cell] = []
                 antennas[cell].append((data.index(row), row.index(cell)))
-    print(antennas)
     return antennas
 
 
-def get_antinodes(a: tuple, b: tuple):
-    node_y, node_x = a[0], a[1]
-    y, x = b[0], b[1]
-    diff_y, diff_x = node_y - y, node_x - x
-    node_1 = y - diff_y, x - diff_x
-    node_2 = y + diff_y * 2, x + diff_x * 2
-    return node_1, node_2
-
-
-def is_valid_antinode(antinode: tuple, bounds: tuple):
+def is_valid(antinode: tuple, bounds: tuple):
     return 0 <= antinode[0] < bounds[0] and 0 <= antinode[1] < bounds[1]
 
 
-def part1(data):
-    bounds = (len(data), len(data[0]))
+def get_antinodes(node_1: tuple, node_2: tuple, bounds: tuple, harmonic: bool):
+    # shifts are made relative to first node, key is the start value for n
+    shifts = {
+        '0': lambda a, b, n: a - b * (2 + n),
+        '1': lambda a, b, n: a + b * n
+    }
+
+    first_y, first_x = node_1
+    second_y, second_x = node_2
+    diff_y, diff_x = first_y - second_y, first_x - second_x
+
+    antinodes = []
+    if harmonic:
+        antinodes.extend([node_1, node_2])
+
+    for k, shift in shifts.items():
+        i = int(k)
+        while True:
+            antinode = shift(first_y, diff_y, i), shift(first_x, diff_x, i)
+            if is_valid(antinode, bounds):
+                antinodes.append(antinode)
+                if not harmonic:
+                    break
+                i += 1
+            else:
+                break
+
+    return antinodes
+
+
+def count_antinodes(data, harmonic: bool = False):
+    bounds = len(data), len(data[0])
     antennas = get_antennas(data)
     antinodes = set()
 
-    for antenna in antennas:
-        positions = antennas[antenna]
-        for i in range(len(positions)):
-            next_antennas = positions[i+1:]
-            for node in next_antennas:
-                antinodes.update([el for el in get_antinodes(positions[i], node) if is_valid_antinode(el, bounds)])
+    for positions in antennas.values():
+        for i, n1 in enumerate(positions):
+            antinodes.update(*[get_antinodes(n1, n2, bounds, harmonic) for n2 in positions[i+1:]])
 
-    # show_map(data)
     return len(antinodes)
 
 
+def part1(data):
+    return count_antinodes(data)
+
+
 def part2(data):
-    return 0
+    return count_antinodes(data, harmonic=True)
 
 
 if __name__ == '__main__':
-    aoc_data = get_data(aoc.SOURCE.EXAMPLE)
+    aoc_data = get_data(aoc.SOURCE.INPUT)
     aoc.benchmark('Part 1', partial(part1, aoc_data))
     aoc.benchmark('Part 2', partial(part2, aoc_data))
