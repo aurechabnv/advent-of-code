560c8fc9203dd7e9df30b57394b8ab8b1b1f3a48
aurechabnv <git@keltpoint.com>
2024-12-22 15:55:01 +0100

    Day 22 - Part 2

--- 2024/day_22/day_22.py
+++ 2024/day_22/day_22.py
@@ -1,12 +1,13 @@
 # Day 22: Monkey Market
 
+from collections import defaultdict
 from functools import partial
 
 import aoc
 
 
-def get_data(source):
-    data = aoc.get_data(src=source, day=22, offset=1)
+def get_data(source, offset=5):
+    data = aoc.get_data(src=source, day=22, offset=offset)
     return list(map(int, data.splitlines()))
 
 
@@ -19,8 +20,8 @@ def get_secret_number(number):
 
 def part1(data):
     total = 0
-    for number in data:
-        secret_number = number
+    for buyer in data:
+        secret_number = buyer
         for _ in range(2000):
             secret_number = get_secret_number(secret_number)
         total += secret_number
@@ -28,7 +29,29 @@ def part1(data):
 
 
 def part2(data):
-    return 0
+    sequences = defaultdict(int)
+
+    for buyer in data:
+        secret_number = buyer
+        buyer_numbers = [(secret_number % 10, 0)]
+        known_sequences, prices, changes = set(), set(), set()
+
+        # save price and price difference
+        for i in range(2000):
+            secret_number = get_secret_number(secret_number)
+            price = secret_number % 10
+            buyer_numbers.append((price, (price - buyer_numbers[-1][0])))
+
+        # extract sequences and add the corresponding price in the dictionary
+        # process only first occurrence of a sequence per buyer
+        prices, changes = set(zip(*buyer_numbers))
+        for i in range(len(buyer_numbers) - 3):
+            seq = changes[i : i + 4]
+            if seq not in known_sequences:
+                sequences[seq] += prices[i + 3]
+                known_sequences.add(seq)
+
+    return max(sequences.values())
 
 
 if __name__ == '__main__':
--- 2024/day_22/test_day_22.py
+++ 2024/day_22/test_day_22.py
@@ -5,7 +5,7 @@ from day_22 import get_data, part1, part2
 
 
 def test_example_part1():
-    data = get_data(aoc.SOURCE.EXAMPLE)
+    data = get_data(aoc.SOURCE.EXAMPLE, offset=1)
     assert part1(data) == 37327623
 
 def test_input_part1():
@@ -13,12 +13,12 @@ def test_input_part1():
     assert part1(data) == 14180628689
 
 def test_example_part2():
-    data = get_data(aoc.SOURCE.EXAMPLE)
-    assert part2(data) == 0
+    data = get_data(aoc.SOURCE.EXAMPLE, offset=5)
+    assert part2(data) == 23
 
 def test_input_part2():
     data = get_data(aoc.SOURCE.INPUT)
-    assert part2(data) == 0
+    assert part2(data) == 1690
 
 if __name__ == '__main__':
     pytest.main()
