0b4823ca7935235e008dc462c9bcd9b766f6ad65
aurechabnv <git@keltpoint.com>
2024-12-25 18:32:01 +0100

    Day 24 - Part 2 (manual search)

--- 2024/day_24/day_24.py
+++ 2024/day_24/day_24.py
@@ -1,25 +1,28 @@
 # Day 24: Crossed Wires
 
+import itertools
 from functools import partial
 
 import aoc
 
 
-def get_data(source):
-    data = aoc.get_data(src=source, day=24, offset=1).split('\n\n')
+def get_data(source, offset=1):
+    data = aoc.get_data(src=source, day=24, offset=offset).split('\n\n')
+    return data
+
+
+def init_dicts(data):
     values, operations = dict(), dict()
     for line in data[0].splitlines():
         v = line.split(':')
-        values[v[0]] = v[1]
+        values[v[0]] = v[1].strip()
     for line in data[1].splitlines():
         v = line.replace('&gt;','>').split('->')
         operations[v[1].strip()] = v[0].strip().lower().replace('xor', '!=')
     return values, operations
 
 
-def part1(data):
-    values, operations = data
-
+def perform_operations(values, operations):
     while operations:
         for new_var, op in list(operations.items())[:]:
             val = op.split()[::2]
@@ -27,12 +30,33 @@ def part1(data):
                 values[new_var] = str(int(eval(op.replace(val[0], values[val[0]]).replace(val[1], values[val[1]]))))
                 del operations[new_var]
 
-    z_values = [v for k, v in reversed(sorted(values.items())) if k.startswith('z')]
-    return int(''.join(z_values), 2)
+
+def get_bin_value(values, name):
+    return int(''.join([v for k, v in reversed(sorted(values.items())) if k.startswith(name)]), 2)
+
+
+def part1(data):
+    values, operations = init_dicts(data)
+    perform_operations(values, operations)
+    return get_bin_value(values, 'z')
 
 
 def part2(data):
-    return 0
+    values, operations = init_dicts(data)
+    inverse_values = [
+        ('z24', 'fpq'), # z24 supposed to be set after z23
+        ('nqk', 'z07'), # z07 to be set before z08
+        ('srn', 'z32'), # z32 to be set with xor
+        ('fgt', 'pcp')  # OR operation requires previous AND in init values
+    ]
+    for a, b in inverse_values:
+        op_a, op_b = operations[a], operations[b]
+        operations[a] = op_b
+        operations[b] = op_a
+
+    perform_operations(values, operations)
+
+    return ','.join(sorted(list(itertools.chain(*inverse_values))))
 
 
 if __name__ == '__main__':
--- 2024/day_24/test_day_24.py
+++ 2024/day_24/test_day_24.py
@@ -5,20 +5,20 @@ from day_24 import get_data, part1, part2
 
 
 def test_example_part1():
-    data = get_data(aoc.SOURCE.EXAMPLE)
+    data = get_data(aoc.SOURCE.EXAMPLE, offset=1)
     assert part1(data) == 2024
 
 def test_input_part1():
     data = get_data(aoc.SOURCE.INPUT)
     assert part1(data) == 61886126253040
 
-def test_example_part2():
-    data = get_data(aoc.SOURCE.EXAMPLE)
-    assert part2(data) == 0
+# def test_example_part2():
+#     data = get_data(aoc.SOURCE.EXAMPLE, offset=5)
+#     assert part2(data) == 'z00,z01,z02,z05'
 
 def test_input_part2():
     data = get_data(aoc.SOURCE.INPUT)
-    assert part2(data) == 0
+    assert part2(data) == 'fgt,fpq,nqk,pcp,srn,z07,z24,z32'
 
 if __name__ == '__main__':
     pytest.main()
