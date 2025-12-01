26ec3129025e6e6819ed15d015a61c7d59196fc9
aurechabnv <git@keltpoint.com>
2025-12-01 14:19:40 +0100

    Day 1 - Init

---
+++ 2025/day_01/day_01.py
@@ -0,0 +1,24 @@
+# Day 1: Secret Entrance
+
+from functools import partial
+
+import aoc
+
+
+def get_data(source):
+    data = aoc.get_data(src=source, day=1)
+    return data
+
+
+def part1(data):
+    return 0
+
+
+def part2(data):
+    return 0
+
+
+if __name__ == '__main__':
+    aoc_data = get_data(aoc.SOURCE.EXAMPLE)
+    aoc.benchmark('Part 1', partial(part1, aoc_data))
+    aoc.benchmark('Part 2', partial(part2, aoc_data))
